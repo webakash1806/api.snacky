093c20a82ecf68f2f81ce56458dab0ae7bf7a988
Akash Kumar Singh <90348968+webakash1806@users.noreply.github.com>
2024-10-12 13:53:43 +0530

    Update order.routes.js

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -4,15 +4,15 @@ import { Router } from 'express';
 // Importing various controller functions and middleware from respective files
 import { allOrders, orderData, updateOrder, userOrders } from '../controllers/order.controller.js';
 
-import { isLoggedIn, authorizedUser } from '../middlewares/auth.middleware.js';
+import {  authorizedUser } from '../middlewares/auth.middleware.js';
 
 // Creating a new instance of the Express Router
 const router = Router();
 
-router.post('/add', isLoggedIn, orderData);
-router.get('/all', isLoggedIn, authorizedUser("ADMIN"), allOrders);
-router.get('/my-orders', isLoggedIn, userOrders);
-router.post('/:id', isLoggedIn, authorizedUser("ADMIN"), updateOrder)
+router.post('/add',  orderData);
+router.get('/all',  authorizedUser("ADMIN"), allOrders);
+router.get('/my-orders',  userOrders);
+router.post('/:id',  authorizedUser("ADMIN"), updateOrder)
 
 
 
